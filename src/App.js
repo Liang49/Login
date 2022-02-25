e3baeace4c71908fbdc6d6f70c3b6285e5ae5167
Liang49 <liangzhang2014@gmail.com>
2022-02-24 23:27:04 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <form>
+        <div className="login">
+          <h1>Login</h1>
+          <label>Username</label>
+          <input type="text" name="name" placeholder="Username or Email" />
+        </div>
+        <div className="password">
+          <label>Password</label>
+          <input type="text" name="password" placeholder="Password" />
+        </div>
+        <button>Login</button>
+      </form>
+    </div>
+  );
+}
